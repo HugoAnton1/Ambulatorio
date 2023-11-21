029cb95a887b256982ca64c6b763ad6047fd03fb
Hugo Antón Cuadrado <Hugoacacademico@gmail.com>
2023-11-21 10:14:00 +0100

    Conexión y tablas realizadas

---
+++ conecta.php
@@ -0,0 +1,32 @@
+<?php
+function getConexion()
+{
+    //Indicamos a que servidor nos vamos a conectar, el usuario y la contraseña
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    //Realizamos la conexion
+    $conexion = mysqli_connect($servername, $username, $password);
+
+    if (!$conexion) {
+        die("Conexión fallida: " . mysqli_connect_error());
+    } else {
+        echo "Conectado";
+        //Creamos la BD
+        $sql = "CREATE DATABASE IF NOT EXISTS Ambulatorio";
+        if (mysqli_query($conexion, $sql)) { // Lanzar BD contra el servidor
+            echo "Base de datos creada con éxito";
+
+            // Seleccionar la base de datos
+            $base_datos = 'Ambulatorio';
+            $conexion->select_db($base_datos);
+
+            // No cerrar la conexión aquí
+            return $conexion;
+        } else {
+            echo "Error al crear la base de datos";
+            mysqli_error($conexion); //Muestra el código de error
+        }
+    }
+}
+$conexion = getConexion();
---
+++ crear_tablas.php
@@ -0,0 +1,47 @@
+<?php
+//Conexión a la base de datos
+require_once("conecta.php");
+$conexion = getConexion();
+$base_datos = 'biblioteca';
+$conexion->select_db($base_datos);
+//Creación de las tablas 
+$sql_consulta = "CREATE TABLE IF NOT EXISTS consulta (
+    id_consulta INT AUTO_INCREMENT PRIMARY KEY,
+    id_medico INT NOT NULL,
+    CONSTRAINT fk_medico FOREIGN KEY (id_medico) REFERENCES medico(id),
+    id_paciente INT NOT NULL,
+    CONSTRAINT fk_paciente FOREIGN KEY (id_paciente) REFERENCES paciente(id),
+    Fecha_consulta DATE NOT NULL,
+    Diagnosticos VARCHAR(255) NOT NULL,
+    Sintomatologia VARCHAR(255) NOT NULL
+)";
+
+$sql_medicamento = "CREATE TABLE IF NOT EXISTS medicamento (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    medicamento VARCHAR(255) NOT NULL,
+)";
+
+$sql_receta = "CREATE TABLE IF NOT EXISTS receta (
+    id_medicamento INT NOT NULL,
+    id_consulta INT NOT NULL,
+    CONSTRAINT fk_medicamento FOREIGN KEY (id_medicamento) REFERENCES lectores(id),
+    CONSTRAINT fk_consulta FOREIGN KEY (id_consulta) REFERENCES libros(id_consulta),
+    Posologia VARCHAR(255) NOT NULL,
+    Fecha_fin DATE NOT NULL
+)";
+
+$sql_medico = "CREATE TABLE IF NOT EXISTS medico (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    apellido VARCHAR(255) NOT NULL,
+    especialidad VARCHAR(255) NOT NULL
+)";
+
+$sql_paciente = "CREATE TABLE IF NOT EXISTS paciente (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    DNI VARCHAR(9) NOT NULL,
+    nombre VARCHAR(255) NOT NULL,
+    apellidos VARCHAR(255) NOT NULL,
+    genero VARCHAR(255) NOT NULL,
+    Fecha_nac DATE NOT NULL,
+    )";
