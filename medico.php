2e8b31b100f1f86df7544193d039e2eafd44f627
Hugo Antón Cuadrado <Hugoacacademico@gmail.com>
2023-11-29 09:09:25 +0100

    Matojo

--- medico.php
+++ medico.php
@@ -4,10 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Medico</title>
-    <link rel="icon" href="/images/ambulatorio.ico" type="image/x-icon">
 </head>
 <body>
-    <h1>Ambulatorio Quintanilla del Montijo</h1>
-    <img src="/images/ambulatorio.ico" alt="Logo del ambulatorio">
+    <h1>Ambulatorio Quintanilla del Matojo</h1>
+    
 </body>
 </html>
--- paciente.php
+++ paciente.php
@@ -5,12 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Paciente</title>
-    <link rel="icon" href="/images/ambulatorio.ico" type="image/x-icon">
 </head>
 
 <body>
-    <h1>Ambulatorio Quintanilla del Montijo</h1>
-    <img src="/images/ambulatorio.ico" alt="Logo del ambulatorio">
+    <h1>Ambulatorio Quintanilla del Matojo</h1>
     <?php
     require_once("conecta.php");
     $conexion = getConexion();
